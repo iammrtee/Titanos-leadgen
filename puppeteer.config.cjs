68f9792c8af0bece9344e0e7d2cd04f42a171057
Admin <tazrt37@gmail.com>
2026-03-11 21:31:22 +0000

    Fix deployment: Align Puppeteer config with browser_bin

--- puppeteer.config.cjs
+++ puppeteer.config.cjs
@@ -5,5 +5,5 @@ const { join } = require('path');
  */
 module.exports = {
     // Changes the cache location for Puppeteer.
-    cacheDirectory: join(__dirname, 'puppeteer_cache'),
+    cacheDirectory: join(__dirname, 'browser_bin'),
 };
